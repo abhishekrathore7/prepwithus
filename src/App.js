6f01babb45a29412fe4a54da1925022ef3a81988
RATHORE Abhishek <abhishek.rathore2@soprasteria.com>
2024-03-09 20:12:44 +0530

    Setup the front-end of Login page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Login from './Components/loginForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  return(
+    <div className='flex items-center justify-center mt-3 w-full h-100'>
+    <div>
+      <Login/>
     </div>
+  </div>
   );
 }
 
